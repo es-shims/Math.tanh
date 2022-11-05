0e2db0b9d9aa2a1daafbdffe11cd07908015c950
Jordan Harband <ljharb@gmail.com>
2022-11-04 22:43:59 -0700

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `aud`, `auto-changelog`, `functions-have-names`, `tape`

--- polyfill.js
+++ polyfill.js
@@ -5,13 +5,14 @@
 var implementation = require('./implementation');
 
 module.exports = function getPolyfill() {
-	var native = Math.tanh;
-	if (!native
+	var original = Math.tanh;
+	if (
+		!original
 		// Chrome 40 has an imprecise Math.tanh with very small numbers
-		|| native(-2e-17) !== -2e-17
+		|| original(-2e-17) !== -2e-17
 	) {
 		return implementation;
 	}
 
-	return native;
+	return original;
 };
